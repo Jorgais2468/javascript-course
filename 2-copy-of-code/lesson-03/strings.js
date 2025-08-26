5a195e4e916eb71e84b38d41b0a26c5156a940f1
Jorge <jorgescalante3@gmail.com>
2025-08-25 23:00:00 -0700

    Copy of code and exercise solutions - Lesson 03

---
+++ 2-copy-of-code/lesson-03/strings.js
@@ -0,0 +1,32 @@
+// String literal
+'hello'
+alert('hello');
+
+// String concatenation
+'some' + 'text'
+'some' + 'more' + 'text'
+
+// Typeof operator
+typeof 2
+typeof 'hello'
+
+// String concatenation with numbers
+'hello' + 3
+'$' + 20.95 + 7.99
+'$' + (20.95 + 7.99)
+'$' + (2095 + 799) / 100
+
+'Items (' + (1 + 1) + '): ' + '$' + (2095 + 799) / 100
+alert('Items (' + (1 + 1) + '): ' + '$' + (2095 + 799) / 100);
+
+// Ways to create a string
+'hello'
+"hello"
+`hello` // backticks interpolation
+"I'm learning Javascript"
+'I\'m learning Javascript'
+alert('some\ntext')
+
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+`some
+text`
